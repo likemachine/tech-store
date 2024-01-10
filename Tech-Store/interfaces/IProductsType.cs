95b87e3edc5e79a0eec54e0ad413f0d31433df4d
Bogdan <boggzon@gmail.com>
2024-01-11 02:58:49 +0300

    initial commit

---
+++ Tech-Store/interfaces/IProductsType.cs
@@ -0,0 +1,11 @@
+using TechStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechStore.interfaces{
+    public interface IProductsType{
+        IEnumerable<Models.Type> AllTypes { get; }
+    }
+}
